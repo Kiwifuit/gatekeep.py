6e68c4a2f758048648e658cb9ccacd1fe8a1f391
Kiwifuit <mahkiwi123@gmail.com>
2024-08-08 11:55:50 +0800

    impl: `init_db`

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 from dotenv import load_dotenv
 from os import environ
 
+from postgres import connect_db, init_db
+
+DATABASE = connect_db()
+
 if __name__ == "__main__":
     load_dotenv()
+    init_db(DATABASE.cursor())
 
     print(f"The bot token is: {environ["BOT_TOKEN"]!r}")
--- src/postgres.py
+++ src/postgres.py
@@ -1,11 +1,65 @@
 from os import environ
-from psycopg import connect, Connection
+from psycopg import connect, Connection, Cursor
 
 
 def connect_db() -> Connection:
+    """
+    Creates a connection to Gatekeep's database
+
+    Returns
+    -------
+    Connection
+        Connection to the database
+    """
     return connect(
         host=environ["PSQL_HOST"],
         database=environ["PSQL_NAME"],
         user=environ["PSQL_USER"],
         password=environ["PSQL_PASS"],
     ).cursor()
+
+
+def init_db(db: Cursor):
+    db.execute(
+        """
+        CREATE TABLE users (
+        id UUID DEFAULT gen_random_uuid(),
+        discord BIGINT NOT NULL UNIQUE,
+        gcash CHAR(11) NOT NULL UNIQUE,
+
+        PRIMARY KEY (id)
+        );
+        """
+    )
+
+    db.execute(
+        """
+        CREATE TABLE workers (
+        id UUID DEFAULT gen_random_uuid(),
+        discord BIGINT NOT NULL UNIQUE,
+        able BOOLEAN DEFAULT true
+        );
+        """
+    )
+
+    db.execute(
+        """
+        CREATE TABLE jobs (
+        uid UUID NOT NULL,
+        wid UUID,
+        jid SMALLSERIAL NOT NULL,
+        title VARCHAR(64) NOT NULL,
+        content TEXT NOT NULL,
+        payment MONEY NOT NULL,
+        completed BOOLEAN DEFAULT false,
+
+        PRIMARY KEY(jid),
+        CONSTRAINT job_owner
+            FOREIGN KEY (uid)
+            REFERENCES users(id)
+        CONSTRAINT job_taker
+            FOREIGN KEY (wid)
+            REFERENCES workers(id)
+        );
+        """
+    )

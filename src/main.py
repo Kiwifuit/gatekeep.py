2705de245e3aec31e63b3d008f80e179b2839f46
donut-bool <177645229+donut-bool@users.noreply.github.com>
2024-08-08 12:28:13 +0800

    Added /createrequest command to send DM to user

--- src/main.py
+++ src/main.py
@@ -1,9 +1,36 @@
 from dotenv import load_dotenv
 from os import environ
-import discord
 
-if __name__ == "__main__":
-    load_dotenv()
-    
-    # burger
-    print(f"The bot token is: {environ["BOT_TOKEN"]!r}")
+import discord
+from discord import app_commands
+
+load_dotenv()
+
+BOT_TOKEN = environ["BOT_TOKEN"]
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@tree.command(
+    name="createrequest",
+    description="Creates a request",
+    guild=discord.Object(id=1270951090669490207)
+)
+async def first_command(interaction: discord.Interaction):
+    user = interaction.user
+    try:
+        await user.send("Hello User, Please state your request. Ensure your request is both descriptive and concise for a better response.")
+        await interaction.response.send_message("I've sent you a DM with further instructions!", ephemeral=True)
+    except discord.Forbidden:
+        await interaction.response.send_message("I couldn't send you a DM. Please check your DM settings.", ephemeral=True)
+@client.event
+async def on_ready():
+    await tree.sync(guild=discord.Object(id=1270951090669490207))
+    print("Ready!")
+
+client.run(BOT_TOKEN)

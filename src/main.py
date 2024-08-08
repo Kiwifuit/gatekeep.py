3864349f100fa4171649dcbbd36e41339b71450a
donut-bool <177645229+donut-bool@users.noreply.github.com>
2024-08-08 16:32:30 +0800

    added regex when asking for user's number

--- src/main.py
+++ src/main.py
@@ -1,28 +1,34 @@
 from dotenv import load_dotenv
 from os import environ
-
+import re
 import discord
 from discord import app_commands
 
-
+PATTERN = r"^(09\d{9}|09\d{2} \d{3} \d{4}|639\d{9}|639\d{2} \d{3} \d{4}|\+639\d{9}|\+639\d{2} \d{3} \d{4})$"
 GUILD_ID = "1270951090669490207"
 
 intents = discord.Intents.default()
+intents.messages = True
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
+
+active_users = {}
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+    await tree.sync(guild=discord.Object(id=GUILD_ID))
+    print("Ready!")
 
 @tree.command(
     name="createrequest",
-    description="Creates a request",
+    description="Creates a request for the user to provide their GCash number.",
     guild=discord.Object(id=GUILD_ID)
 )
 async def create_request(interaction: discord.Interaction):
     user = interaction.user
     user_id = user.id
+    active_users[user_id] = True
     try:
         await user.send(f"Hello User `{user_id}`, Please provide your valid `GCash number` before proceeding.")
         await interaction.response.send_message("I've sent you a DM with further instructions!", ephemeral=True)
@@ -30,9 +36,17 @@ async def create_request(interaction: discord.Interaction):
         await interaction.response.send_message("I couldn't send you a DM. Please check your DM settings.", ephemeral=True)
 
 @client.event
-async def on_ready():
-    await tree.sync(guild=discord.Object(id=GUILD_ID))
-    print("Ready!")
+async def on_message(message: discord.Message):
+    if isinstance(message.channel, discord.DMChannel) and message.author != client.user:
+        if message.author.id in active_users:
+            if re.match(PATTERN, message.content):
+                print("Success: Valid phone number received.")
+                await message.channel.send("Success: Your number is valid.")
+                del active_users[message.author.id]
+            else:
+                await message.channel.send("Invalid number. Please provide a valid Philippine phone number.")
+        else:
+            pass
 
 def main():
     load_dotenv()
